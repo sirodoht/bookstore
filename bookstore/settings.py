3289af609d670d79aa04ee88a6d8f7cb67be73c1
Theodore Keloglou <zf@sirodoht.com>
2026-02-28 19:38:23 +0000

    feat: add STATIC_ROOT configuration for production deployment

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -123,6 +123,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "static"
+
 
 # Stripe
 STRIPE_SECRET_KEY = os.environ.get("STRIPE_SECRET_KEY")
